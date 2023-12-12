8c260064b16dc2884ec4aa3f70d557d21e29a71d
Shivam Mishra <mshivam019@gmail.com>
2023-12-12 12:48:15 +0530

    part 2 of day 1 advent of code

--- advent of code/q1.java
+++ advent of code/q1.java
@@ -1,27 +1,41 @@
 import java.util.Scanner;
-class q1 {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        String num = "";
-        while (sc.hasNext()) {
-            String numstring = sc.nextLine();
-            num = "";
-           for(int i = 0; i < numstring.length(); i++) {
-              if(Character.isDigit(numstring.charAt(i))) {
-                  num += numstring.charAt(i);
-                  break;
-              }
+
+public class q1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        var sum = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            var calibNums = getCalibNums(line);
+            sum += Integer.parseInt(calibNums);
+        }
+
+        System.out.println("Output: " + sum);
+        scanner.close();
+
+    }
+
+    private static String getCalibNums(String line) {
+        String[] digits = { "-", "one", "two", "three", "four", "five", "six",
+                "seven", "eight", "nine" };
+        var nums = "";
+        var chars = line.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isDigit(chars[i])) {
+                nums += chars[i];
+                continue;
             }
-            for(int i = numstring.length() - 1; i >= 0; i--) {
-                if(Character.isDigit(numstring.charAt(i))) {
-                    num += numstring.charAt(i);
+            var substr = line.substring(i, Math.min(5 + i, line.length()));
+            for (int j = 0; j < digits.length; j++) {
+                if (substr.startsWith(digits[j])) {
+                    i += digits[j].length() - 2;
+                    nums += j;
                     break;
                 }
             }
-            sum += Integer.parseInt(num);
         }
-        System.out.println(sum);
-        sc.close();
+        return "" + nums.charAt(0) + nums.charAt(nums.length() - 1);
     }
 }
